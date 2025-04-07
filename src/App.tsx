fcae774db20901297e77b1f700c38cf876df85b2
종빈 <whddkrak@gmail.com>
2025-04-07 22:22:45 +0900

    feat: 화면 업데이트하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import {useState} from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   function handleClick() {
-    alert('You clicked me!');
+    return () => { // 클로저 함수
+      setCount(count + 1);
+    }
   }
 
   return (
     // 이벤트 핸들러 함수를 호출하지 않고 전달만 하면 됩니다.
-    <button onClick={handleClick}>
-      Click me
+    <button onClick={handleClick()}>
+      {count}
     </button>
   )
 }
