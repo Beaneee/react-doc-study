a946f63fcf96173e69b8f89c829a8832d7c308aa
종빈 <whddkrak@gmail.com>
2025-04-07 22:13:25 +0900

    feat: 리스트 렌더링하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 function App() {
-  const isLoggedIn: number = 0;
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
 
   return (
-    <div>
-      <div>
-        {/* JavaScript에서 a && b는 */}
-        {/* a가 truthy이면 → b를 반환 */}
-        {/* a가 falsy이면 → a를 반환 */}
-        {/* 따라서 0을 출력하게 된다. */}
-        {isLoggedIn && <></>}
-      </div>
-    </div>
+    <ul>
+      {products.map(product =>
+        // 목록의 각 항목에 대해, 형제 항목 사이에서 해당 항목을 고유하게 식별하는 문자열 또는 숫자를 전달해야 합니다.
+        // React는 나중에 항목을 삽입, 삭제 또는 재정렬할 때 어떤 일이 일어났는지 알기 위해 key를 사용합니다.
+        <li key={product.id}>
+          {product.title}
+        </li>
+      )}
+    </ul>
   )
 }
 
