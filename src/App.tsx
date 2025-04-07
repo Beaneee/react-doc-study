4ff25572af2f8f58170dee3c9e8cc403b798bf30
종빈 <whddkrak@gmail.com>
2025-04-07 21:32:04 +0900

    feat: 데이터 표시하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-// React 컴포넌트의 이름은 항상 대문자로 시작해야 하고 HTML 태그는 소문자로 시작해야 합니다.
-function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
-}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
 function App() {
+
   return (
-    // 컴포넌트는 여러 개의 JSX 태그를 반환할 수 없습니다.
-    // <div>...</div> 또는 빈 <>...</> 래퍼와 같이 공유되는 부모로 감싸야 합니다.
     <div>
-      <h1>Welcome to my app</h1>
-      {/* JSX에서는 <br />같이 태그를 닫아야 합니다. */}
-      <MyButton/>
+      {/* 중괄호를 사용하면 코드에서 일부 변수를 삽입하여 사용자에게 표시할 수 있도록
+      자바스크립트로 “이스케이프 백(Escape Back)” -> 자바스크립트 표현식을 삽입할 수 있습니다.*/}
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        // style={{}}은 특별한 문법이 아니라 style={ } JSX 중괄호 안에 있는 일반 {} 객체입니다.
+        // 스타일이 자바스크립트 변수에 의존하는 경우 style 어트리뷰트를 사용할 수 있습니다.
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </div>
   )
 }
