a380e57524380b24de9fff73a9b64b6cce2c7331
종빈 <whddkrak@gmail.com>
2025-04-07 21:13:00 +0900

    feat: 컴포넌트 생성 및 중첩하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-function App() {
-
+// React 컴포넌트의 이름은 항상 대문자로 시작해야 하고 HTML 태그는 소문자로 시작해야 합니다.
+function MyButton() {
   return (
-    <>
+    <button>
+      I'm a button
+    </button>
+  );
+}
 
-    </>
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton/>
+    </div>
   )
 }
 
