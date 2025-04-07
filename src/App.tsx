1f5a6a5011424da3e411431e234173c7db9f840e
종빈 <whddkrak@gmail.com>
2025-04-07 22:18:17 +0900

    feat: 이벤트에 응답하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 function App() {
-  const products = [
-    { title: 'Cabbage', id: 1 },
-    { title: 'Garlic', id: 2 },
-    { title: 'Apple', id: 3 },
-  ];
+  function handleClick() {
+    alert('You clicked me!');
+  }
 
   return (
-    <ul>
-      {products.map(product =>
-        // 목록의 각 항목에 대해, 형제 항목 사이에서 해당 항목을 고유하게 식별하는 문자열 또는 숫자를 전달해야 합니다.
-        // React는 나중에 항목을 삽입, 삭제 또는 재정렬할 때 어떤 일이 일어났는지 알기 위해 key를 사용합니다.
-        <li key={product.id}>
-          {product.title}
-        </li>
-      )}
-    </ul>
+    // 이벤트 핸들러 함수를 호출하지 않고 전달만 하면 됩니다.
+    <button onClick={handleClick}>
+      Click me
+    </button>
   )
 }
 
