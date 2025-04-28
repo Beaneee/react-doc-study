d4893da64c34dcd8f98932e2e03f5945a401ced9
종빈 <whddkrak@gmail.com>
2025-04-28 23:06:43 +0900

    feat: 사용자와 상호작용하는 컴포넌트 만들기

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,20 @@
-const Square = ({ value }) => {
-  return <button className="square">{value}</button>;
+import {useState} from "react";
+
+function Square() {
+  const [value, setValue] = useState<string | null>(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default Square;
