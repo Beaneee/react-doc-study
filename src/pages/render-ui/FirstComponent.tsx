0bc89750a2cda785d04d189d6da041906302e34e
종빈 <whddkrak@gmail.com>
2025-04-30 22:42:25 +0900

    feat: 컴포넌트 정의하기

--- src/pages/render-ui/FirstComponent.tsx
+++ src/pages/render-ui/FirstComponent.tsx
@@ -1,8 +1,14 @@
 const FirstComponent = () => {
   return (
-    <div style={{ fontSize: '16px', color: '#555' }}>
-      첫 번째 컴포넌트
+    <div style={{fontSize: '16px', color: '#555', display: 'flex', flexDirection: 'column'}}>
+      <span>첫 번째 컴포넌트</span>
+      <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+        width={300}
+      />
     </div>
+
   );
 };
 
