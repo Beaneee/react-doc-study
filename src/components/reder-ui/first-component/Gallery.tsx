9987eaac9b16b21b9e8ba03fe8ac239b63a5bd46
종빈 <whddkrak@gmail.com>
2025-04-30 23:03:38 +0900

    feat: 컴포넌트 사용하기

---
+++ src/components/reder-ui/first-component/Gallery.tsx
@@ -0,0 +1,14 @@
+import Profile from "@/components/reder-ui/first-component/Profile.tsx";
+
+const Gallery = () => {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+export default Gallery;
--- src/pages/render-ui/FirstComponent.tsx
+++ src/pages/render-ui/FirstComponent.tsx
@@ -1,12 +1,10 @@
+import Gallery from "@/components/reder-ui/first-component/Gallery.tsx";
+
 const FirstComponent = () => {
   return (
     <div style={{fontSize: '16px', color: '#555', display: 'flex', flexDirection: 'column'}}>
       <span>첫 번째 컴포넌트</span>
-      <img
-        src="https://i.imgur.com/MK3eW3Am.jpg"
-        alt="Katherine Johnson"
-        width={300}
-      />
+      <Gallery />
     </div>
 
   );
