eda3c9ae12eeef5e13793ecbef6bf0315b988b7a
종빈 <whddkrak@gmail.com>
2025-05-08 22:45:22 +0900

    feat: 컴포넌트 import 및 export 하기

--- src/components/reder-ui/first-component/Gallery.tsx
+++ src/components/reder-ui/first-component/Gallery.tsx
@@ -11,4 +11,6 @@ const Gallery = () => {
   );
 }
 
+// JavaScript에서는 default와 named export라는 두 가지 방법으로 값을 export 합니다.
+// 다만 한 파일에서는 하나의 default export만 존재할 수 있고 named export는 여러 개가 존재할 수 있습니다.
 export default Gallery;
